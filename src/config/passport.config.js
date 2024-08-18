32f3945e8bb9db8d6cccda8fff590a5ce016373f
Administrador <Administrador@POLLUXC04>
2024-08-18 13:30:21 -0300

    ajustes de deploy26

--- src/config/passport.config.js
+++ src/config/passport.config.js
@@ -160,9 +160,9 @@ const initializeStrategy = () => {
         }
     }))
 
-    const client_ID = process.env.CLIENT_ID //|| 'Iv1.6d669ffe54ac6555'
-    const client_SECRET = process.env.CLIENT_SECRET //|| '28cf37c5290e1cb5ccbc9138c679536918cdef49'
-    const callback_URL = process.env.CALLBACK_URL //|| 'http://localhost:8080/api/sessions/githubcallback'
+    const client_ID = process.env.CLIENT_ID          || 'Iv1.6d669ffe54ac6555'
+    const client_SECRET = process.env.CLIENT_SECRET  || '28cf37c5290e1cb5ccbc9138c679536918cdef49'
+    const callback_URL = process.env.CALLBACK_URL    || 'http://localhost:8080/api/sessions/githubcallback'
     passport.use('github', new GithubStrategy({
         clientID: client_ID,
         clientSecret: client_SECRET,
